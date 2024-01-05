2f9cc06b09a3297dd7ba326ec5ecb1f09f2d57be
TheBitNinja <contact@davidebaraldo.me>
2024-01-05 17:58:13 +0100

    [feat] testDavide.h for test purposes

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
+#include "tests/testDavide.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+	//TestDavide::runTest();
+}
 
 MainWindow::~MainWindow() {}
