e12628f09bd348ad7263278ab381f0f47b7419ea
TheBitNinja <contact@davidebaraldo.me>
2024-01-07 15:33:32 +0100

    [fix] StorageUtility template issues directory creating

--- src/utility/StorageUtility.h
+++ src/utility/StorageUtility.h
@@ -1,6 +1,8 @@
 #ifndef SMARTSENSORS_STORAGEUTILITY_H
 #define SMARTSENSORS_STORAGEUTILITY_H
 #include <QString>
+#include <QFileInfo>
+#include <QDir>
 #include "../model/core/Sensor.h"
 #include "Logger.h"
 class StorageUtility {
@@ -13,7 +15,11 @@ public:
 template <typename T>
 bool StorageUtility::Store(T data, QString filename)
 {
-	QFile file(filename);
+	QFileInfo fi(filename);
+	QDir dir(fi.absoluteDir().absoluteFilePath("."));
+	if (!dir.exists())
+		dir.mkpath(".");
+	QFile file(fi.absoluteFilePath());
 	if (file.open(QIODevice::WriteOnly)) {
 		QDataStream out(&file);
 		out << data;
@@ -27,7 +33,8 @@ bool StorageUtility::Store(T data, QString filename)
 template <typename T>
 T* StorageUtility::Load(QString filename)
 {
-	QFile loadFile(filename);
+	QFileInfo fi(filename);
+	QFile loadFile(fi.absoluteFilePath());
 	if (!loadFile.open(QIODevice::ReadOnly))
 	{
 		Logger::Log(LogLevel::ERROR,__FILE__,__LINE__,__FUNCTION__,QString("Couldn't open save file."));
