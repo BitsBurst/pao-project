c90951454091b8b2a9319fb979a0d97124ea1936
TheBitNinja <contact@davidebaraldo.me>
2024-02-02 15:23:34 +0100

    [fix] not destroying view elements

--- src/view/views/GroupListView.cpp
+++ src/view/views/GroupListView.cpp
@@ -53,8 +53,10 @@ void GroupListView::setItems(const QVector<AbstractItem*>& items)
 {
     scroll_area_layout->deleteLater();
     scroll_area_list->deleteLater();
-    item_lists_.clear();
-
+	for (auto item : item_lists_) {
+		delete item;
+	}
+	item_lists_.clear();
     scroll_area_list = new QWidget(scroll_area_);
     scroll_area_layout = new QVBoxLayout(scroll_area_list);
     scroll_area_layout->setSpacing(16);
