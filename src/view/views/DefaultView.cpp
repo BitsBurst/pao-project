e4c655c3ce417638a20813e47a7611d12bef72ce
Jelox <monettiluca03@gmail.com>
2024-02-04 17:08:26 +0100

    [fix] New Default View

--- src/view/views/DefaultView.cpp
+++ src/view/views/DefaultView.cpp
@@ -6,13 +6,29 @@
 DefaultView::DefaultView(QWidget* parent)
     : AbstractView(CustomElements::getCustomLayoutPrototype(SINGLE_SPACING), parent)
 {
-    QLabel* title = new QLabel("Titolo di Default");
-    QLabel* description = new QLabel("Una descrizione per il titolo di Default");
+    QLabel* title = new QLabel("SmartSensor");
+    QLabel* description = new QLabel("Seleziona un sensore per visualizzare la simulazione dei dati");
+
+    QLabel* key_1 = new QLabel("<b>Ctrl + o</b> : Apri una simulazione esistente");
+    QLabel* key_2 = new QLabel("<b>Ctrl + s</b> : Esporta la simulazione corrente");
+    QLabel* key_3 = new QLabel("<b>Ctrl + n</b> : Crea un nuovo sensore");
+    QLabel* key_4 = new QLabel("<b>Ctrl + Shift + n</b> : Crea una nuova categoria");
 
     title->setFont(CustomElements::getFontH2());
     description->setFont(CustomElements::getFontParagraph());
+    key_1->setFont(CustomElements::getFontParagraph());
+    key_2->setFont(CustomElements::getFontParagraph());
+    key_3->setFont(CustomElements::getFontParagraph());
+    key_4->setFont(CustomElements::getFontParagraph());
+
+    layout_->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 
     layout_->addWidget(title);
     layout_->addWidget(description);
+    layout_->addItem(new QSpacerItem(0,20));
+    layout_->addWidget(key_1);
+    layout_->addWidget(key_2);
+    layout_->addWidget(key_3);
+    layout_->addWidget(key_4);
     static_cast<QVBoxLayout*>(layout_)->addStretch();
 }
